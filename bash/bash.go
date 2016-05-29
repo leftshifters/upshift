f20a557582aa379d7d49517e5d164cd8905ea53f
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-05-29 06:38:58 +0530

    Reached a breaking point. This is not going to end anytime soon. [6h36m0s]

--- bash/bash.go
+++ bash/bash.go
@@ -3,55 +3,143 @@ package bash
 import (
 	"bufio"
 	"bytes"
-	"fmt"
-	"os"
+	"errors"
+	log "github.com/Sirupsen/logrus"
+	// "io"
 	"os/exec"
 	"strings"
 )
 
+// Create a struct for errors
+type BashErrors struct {
+	errorMessage string
+}
+
+func (e *BashErrors) Error() string {
+	return e.errorMessage
+}
+
+// Get started with init
 func init() {
-	fmt.Println("Initializing Bash Package")
+
+}
+
+// Expect multiple commands
+type Command struct {
+	name string
+	args []string
+	next string // This defines how the pipe would work, it can be | or && for now
+	cmd  *exec.Cmd
 }
 
 // Execute arbitary bash script like "ls -la"
-func Bash(command string) string {
+func Bash(command string) (string, error) {
 
+	// Inspiration from http://nathanleclaire.com/blog/2014/12/29/shelled-out-commands-in-golang/
 	var outputBuffer bytes.Buffer
+	var errorBuffer bytes.Buffer
 
-	// Literally flicked from http://nathanleclaire.com/blog/2014/12/29/shelled-out-commands-in-golang/
+	commands, err := createCommandQueue(command)
+	if err != nil {
+		return "", errors.New(err.Error())
+	}
+	// log.Info("CMDS", commands)
+
+	if len(commands) == 0 {
+		return "", &BashErrors{"There are no commands to process"}
+	}
+
+	errScanner := bufio.NewScanner(&errorBuffer)
+	go func() {
+		log.Info("I ran too")
+		for errScanner.Scan() {
+			log.Info("y", errScanner.Text())
+		}
+	}
+
+	setupPipes(commands, errorBuffer)
+	runCommands(commands, errorBuffer)
+
+	return outputBuffer.String(), nil
+}
+
+func runCommands(commands []Command, errorBuffer bytes.Buffer) {
+	for i, command := range commands {
+		log.Info(i, command.cmd.Stdin)
+		log.Info(i, command.cmd.Stdout)
+
+		if err := command.cmd.Start(); err != nil {
+			log.Error(i, "Problem starting command")
+		}
+
+		if err := command.cmd.Wait(); err != nil {
+			log.Error(i, "Problem waiting for command ", err.Error())
+		}
+	}
+}
+
+func setupPipes(commands []Command, errorBuffer bytes.Buffer) {
+	log.Info("CMD", commands)
+	for i, command := range commands[:len(commands)-1] {
+		out, _ := command.cmd.StdoutPipe()
+		command.cmd.Stderr = &errorBuffer
+		commands[i+1].cmd.Stdin = out
+
+		scanner := bufio.NewScanner(out)
+		go func() {
+			log.Info("I did run")
+			for scanner.Scan() {
+				log.Info("x", scanner.Text())
+			}
+		}()
+	}
+}
+
+func createCommandQueue(command string) ([]Command, error) {
+
+	// Create an empty list of commands
+	commands := []Command{}
+
+	// Parse the command string to create one big array with all commands and args
 	commandParams := strings.Fields(command)
 
+	// Check if there are no commands, and just return if it is
 	if len(commandParams) == 0 {
-		// TODO : Find better ways to show and pass errors
-		fmt.Println("There was nothing in the command")
-		return outputBuffer.String()
+		// return commands, errors.New("The command was empty")
+		return commands, &BashErrors{"The command was empty"}
 	}
 
-	cmd := exec.Command(commandParams[0], commandParams[1:]...)
-	cmdReader, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
-		os.Exit(1)
-	}
+	//commands = append(commands, Command{name: commandParams[0], args: commandParams[1:]})
 
-	scanner := bufio.NewScanner(cmdReader)
-	go func() {
-		for scanner.Scan() {
-			outputBuffer.WriteString(scanner.Text())
+	inCommandSeq := 0
+	tempCommand := Command{}
+
+	for _, param := range commandParams {
+		// Figure out if param is a name, arg or separator
+		if inCommandSeq == 0 {
+			tempCommand.name = param
+		} else if param == "&&" {
+			// Ignore
+			log.WithFields(log.Fields{"package": "Bash", "command": command}).Error("We don't handle && in bash commands, keem them out")
+			return commands, &BashErrors{"Found an unsupported action in the shell script"}
+		} else if param == "|" {
+			tempCommand.next = param
+			tempCommand.cmd = exec.Command(tempCommand.name, tempCommand.args...)
+			// Move tempCommand to commands and get a empty one again
+			commands = append(commands, tempCommand)
+			tempCommand.name = ""
+			tempCommand.args = nil
+			tempCommand.next = ""
+
+			inCommandSeq = -1
+		} else {
+			tempCommand.args = append(tempCommand.args, param)
 		}
-	}()
-
-	err = cmd.Start()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
-		os.Exit(1)
+		inCommandSeq = inCommandSeq + 1
 	}
 
-	err = cmd.Wait()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error waiting for command", err)
-		os.Exit(1)
-	}
+	tempCommand.cmd = exec.Command(tempCommand.name, tempCommand.args...)
+	commands = append(commands, tempCommand)
 
-	return outputBuffer.String()
+	return commands, nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	log "github.com/Sirupsen/logrus"
+	"os"
 	bash "upshift/bash"
 )
 
+func init() {
+	log.SetOutput(os.Stderr)
+	log.SetLevel(log.DebugLevel)
+}
+
 func main() {
-	bash.Bash("ls -la")
+	// output, err := bash.Bash("echo 134 | grep 134 -c")
+	output, err := bash.Bash("echo 134 && echo 134| grep 134 -c")
+	// output := bash.Bash("xcodebuild -project Deezeno.xcodeproj -scheme deezeno-ios -hideShellScriptEnvironment -sdk iphonesimulator -destination \"platform=iphonesimulator,name=iPhone 6\" -derivedDataPath build | tee \"xcode-build\" | xcpretty")
+	// output := bash.Bash("xcodebuild -project Deezeno.xcodeproj -scheme deezeno-ios -hideShellScriptEnvironment -sdk iphonesimulator -destination \"platform=iphonesimulator,name=iPhone 6\" -derivedDataPath build")
+	// output := bash.Bash("xcodebuild")
+
+	if err != nil {
+		log.Error(err)
+		log.Error(err.code)
+	}
+
+	log.Info("<", output, ">")
+
 }
