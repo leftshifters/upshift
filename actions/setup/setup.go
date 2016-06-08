d096d1dfb81896770f7489645bb857e0aca77cf8
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-06-08 23:26:32 +0530

    added comments for git pull and submodules [3m0s]

--- actions/setup/setup.go
+++ actions/setup/setup.go
@@ -21,6 +21,9 @@ func init() {
 
 }
 
+//
+// Initialize submodules in a project
+//
 func GitSubmodules() (int, bool) {
 	submodulePath, _ := filepath.Abs(".gitmodules")
 	submodulesExist := utils.FileExists(submodulePath)
@@ -65,34 +68,8 @@ func GitSubmodules() (int, bool) {
 	return 0, false
 }
 
-// function GitSubmodules {
-
-//   # Make a TIMESHTAMP for log file
-//   TIMESHTAMP=$(date +%Y%m%d%H%M%S)
-
-//   # Check if .gitmodules exists
-//   if [ -f ".gitmodules" ]; then
-//     # If the file exists, we need to run init and update and catch errors
-//     git submodule init 2>&1 | tee "git-submodule-init-${TIMESHTAMP}.log"
-//     git submodule update 2>&1 | tee "git-submodule-update-${TIMESHTAMP}.log"
-
-//     SUBMODULE_RESULTS=$(<"git-submodule-update-${TIMESHTAMP}.log")
-
-//     if [ $(echo "${SUBMODULE_RESULTS}" | grep "fatal:" -c) -gt 0 ] || [ $(echo "${SUBMODULE_RESULTS}" | grep "error:" -c) -gt 0 ]; then
-//       ShowError
-//       printf "Damn, initializing submodules was ${redColour}not successful${noColour}. You should check this up.\n\n"
-//       next=false
-//     else
-//       printf "\nSubmodules are now ${greenColour}setup${noColour}, one less thing to think about! 🍺\n\n"
-//     fi
-//     # Else Quietly ignore
-//   else
-//     printf "\nIt looks like this project doesn't use ${greenColour}submodules${noColour}.\n\n"
-//   fi
-// }
-
 //
-//
+// Do a git pull on the project based on the defined remote and the branch the user is currently on
 //
 func GitPull() (int, bool) {
 
