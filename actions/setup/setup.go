74229cea0652f9e90e0cf298772dac384c2f64c2
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-06-08 22:54:05 +0530

    Removed inspiration from git pull [1m0s]

--- actions/setup/setup.go
+++ actions/setup/setup.go
@@ -125,39 +125,6 @@ func GitPull() (int, bool) {
 	return 0, false
 }
 
-//   # Make a TIMESHTAMP for log file
-//   TIMESHTAMP=$(date +%Y%m%d%H%M%S)
-
-//   # Check if the branch name is defined
-//   if [ "${gitRepositoryBranch}" != "" ]; then
-
-//     printf "Alright, let's ${greenColour}pull${noColour} the ${gitRepositoryBranch} branch for this repo\n\n"
-
-//     # Alright, let's pull
-//     git pull origin ${gitRepositoryBranch} 2>&1 | tee "git-pull-${TIMESHTAMP}.log"
-
-//     # Load up the results to see if there were any errors
-//     PULL_RESULTS=$(<"git-pull-${TIMESHTAMP}.log")
-//     PULL_RESULTS_FATAL=$(grep "fatal:" -c <<< "${PULL_RESULTS}")
-//     PULL_RESULTS_ERROR=$(grep "error:" -c <<< "${PULL_RESULTS}")
-//     # If there was a fatal error, tell the user there's something wrong
-//     if [ "${PULL_RESULTS_FATAL}" -gt "0" ] || [ "${PULL_RESULTS_ERROR}" -gt "0" ]; then
-//       ShowError
-//       printf "Something went wrong with the pull, you should look this up\n\n"
-//       next=false
-//     else
-//       # All done
-//       printf "\nAll done ${greenColour}baby${noColour}! 🍺.\n\n"
-//     fi
-
-//   else
-//     # The user hasn't added the required keys
-//     ShowError
-//     printf "Dude, you need to add the ${blueColour}gitRepositoryBranch${noColour} for this to work\nYou can get a sample config by running upshift setup config\n\n"
-//     next=false
-//   fi
-// }
-
 //
 // Show help, so that the user knows what to do
 //
