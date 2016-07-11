3397575c153074c5d91f7ba3bdfc9b10160b985e
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-12 00:10:51 +0530

    Added tests for AndroidEmulator

--- actions/androidemulator_test.go
+++ actions/androidemulator_test.go
@@ -1,7 +1,30 @@
 package actions
 
-import "testing"
+import (
+	"testing"
 
-func Test_AndroidEmulator_Launch(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
 
+func Test_AndroidEmulator_All(t *testing.T) {
+	var emulator AndroidEmulator
+
+	devices, err := emulator.ConnectedDevices()
+	t.Log(devices)
+	assert.Equal(t, 0, len(devices))
+	assert.Nil(t, err)
+
+	err = emulator.CreateAVD()
+	assert.Contains(t, err.Error(), "not been implemented")
+
+	avds, err := emulator.AVDsAvailable()
+	t.Log(avds)
+	assert.Equal(t, 0, len(avds))
+	assert.Nil(t, err)
+
+	err = emulator.Launch()
+	assert.Nil(t, err)
+
+	err = emulator.LaunchApp("test", "MainActivity")
+	assert.Nil(t, err)
 }
