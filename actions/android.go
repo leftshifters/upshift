f6185850ea28591944f7315a90a80dcc3c0f40ec
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-12 01:22:25 +0530

    Brought back the scripts, commented in go

--- actions/android.go
+++ actions/android.go
@@ -1,27 +1,25 @@
 package actions
 
-import "upshift/basher"
-
 // Android : Construct for all activities related to the android binary
 type Android struct {
 }
 
 // UpgradeAndroid : Upgrade the android sdk
 func (a *Android) UpgradeAndroid(logPath string) error {
-	var b basher.Basher
-	_, err := b.Run("AndroidUpgradeSDK", []string{logPath})
-	if err != nil {
-		return err
-	}
+	// var b basher.Basher
+	// _, err := b.Run("AndroidUpgradeSDK", []string{logPath})
+	// if err != nil {
+	// 	return err
+	// }
 	return nil
 }
 
 // SetupAndroid : Install the android sdk
 func (a *Android) SetupAndroid(logPath string) error {
-	var b basher.Basher
-	_, err := b.Run("AndroidInstallSDK", []string{logPath})
-	if err != nil {
-		return err
-	}
+	// var b basher.Basher
+	// _, err := b.Run("AndroidInstallSDK", []string{logPath})
+	// if err != nil {
+	// 	return err
+	// }
 	return nil
 }
