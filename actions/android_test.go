a7e0c8e31dbd7adf323256b963bcd35c7cb6fe81
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-12 00:03:00 +0530

    Added tests for Android.go

--- actions/android_test.go
+++ actions/android_test.go
@@ -1,7 +1,17 @@
 package actions
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_Android_All(t *testing.T) {
+	var android Android
 
+	err := android.SetupAndroid("android-setup.log")
+	assert.Nil(t, err)
+
+	err = android.UpgradeAndroid("android-upgrade.log")
+	assert.Nil(t, err)
 }
