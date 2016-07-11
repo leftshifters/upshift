e00107eb1a0353b0b1c1ed361ea8d3fa256ad032
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-11 23:59:06 +0530

    Added tests for actions.go

--- actions/actions_test.go
+++ actions/actions_test.go
@@ -7,8 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_GradleWrapper(t *testing.T) {
+func Test_Actions_GradleWrapper(t *testing.T) {
+	currentWD, _ := os.Getwd()
 	os.Chdir("android-chat")
 	status := GradleWrapper()
 	assert.Equal(t, 0, status)
+	os.Chdir(currentWD)
+}
+
+func Test_Actions_PodInstall(t *testing.T) {
+	currentWD, _ := os.Getwd()
+	os.Chdir(os.Getenv("HOME") + "/code/deezeno-ios")
+	status := PodInstall()
+	assert.Equal(t, 0, status)
+	os.Chdir(currentWD)
+}
+
+func Test_Actions_AndroidBuild(t *testing.T) {
+	currentWD, _ := os.Getwd()
+	os.Chdir(os.Getenv("HOME") + "android-chat")
+	status := AndroidBuild()
+	assert.Equal(t, 0, status)
+	os.Chdir(currentWD)
 }
