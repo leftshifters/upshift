cf6fed710a4eabae311d017a12bd9e07434d4eac
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-13 19:40:59 +0530

    Added test for xcodebuild

--- actions/xcodebuild_test.go
+++ actions/xcodebuild_test.go
@@ -9,19 +9,66 @@ import (
 )
 
 func Test_Xcodebuild_LoadSettings(t *testing.T) {
-	assert.Equal(t, 1, 1)
-
 	// Get the current working directory
 	currentWD, _ := os.Getwd()
 
 	// Move to the new directory
 	os.Chdir(filepath.Join(os.Getenv("HOME"), "code", "deezeno-ios"))
-	newWD, _ := os.Getwd()
-	t.Log(newWD)
 
 	var xcodebuild Xcodebuild
-	xcodebuild.LoadSettings()
-	t.Log(xcodebuild)
+
+	err := xcodebuild.LoadSettings()
+	assert.Equal(t, "project", xcodebuild.Type)
+	assert.Nil(t, err)
+
+	err = xcodebuild.FindSchemes()
+	assert.Nil(t, err)
+	assert.Equal(t, "deezeno-ios", xcodebuild.Scheme)
+
+	err = xcodebuild.Build()
+	assert.Nil(t, err)
+
+	var simulator IOSSimulator
+	simulator.StartSimulator("iPhone 6 (9.3)")
+
+	err = xcodebuild.Run()
+	assert.Nil(t, err)
+
+	err = xcodebuild.InstallCertificates()
+	assert.Nil(t, err)
+
+	err = xcodebuild.Archive()
+	assert.Nil(t, err)
+
+	err = xcodebuild.SetupExportPlist()
+	assert.Nil(t, err)
+
+	err = xcodebuild.ExportIPA()
+	assert.Nil(t, err)
+
+	err = xcodebuild.IncrementBuildNumber()
+	assert.Nil(t, err)
+
+	// Move back to older working directory
+	os.Chdir(currentWD)
+}
+
+func Test_Xcodebuild_SwitchXcode(t *testing.T) {
+	// Get the current working directory
+	currentWD, _ := os.Getwd()
+
+	// Move to the new directory
+	os.Chdir(filepath.Join(os.Getenv("HOME"), "code", "deezeno-ios"))
+
+	var xcodebuild Xcodebuild
+
+	xcodebuild.XcodeVersion = "7.3"
+	err := xcodebuild.SwitchXcode()
+	assert.Nil(t, err)
+
+	xcodebuild.XcodeVersion = "7.3.1"
+	err = xcodebuild.SwitchXcode()
+	assert.Nil(t, err)
 
 	// Move back to older working directory
 	os.Chdir(currentWD)
