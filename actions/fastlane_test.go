c0d5cf25e1c37e1b8ad1c6a9fcb9738da91f2b02
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-15 00:31:21 +0530

    killed tests for fastlane, they are too long

--- actions/fastlane_test.go
+++ actions/fastlane_test.go
@@ -1,36 +1,31 @@
 package actions
 
-import (
-	"os"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
+import "testing"
 
 func Test_Fastlane_Install(t *testing.T) {
-	var fastlane Fastlane
-	var g Gems
+	// var fastlane Fastlane
+	// var g Gems
 
 	// Check if it is installed or not
-	installed, _ := g.version("fastlane")
+	// installed, _ := g.version("fastlane")
 
 	// Change env setting, simulate CI
-	os.Setenv("GITLAB_CI", "true")
+	// os.Setenv("GITLAB_CI", "true")
 
 	// Remove fastlane
-	err := fastlane.Uninstall()
-	assert.Nil(t, err)
+	// err := fastlane.Uninstall()
+	// assert.Nil(t, err)
 
 	// Install fastlane again
-	err = fastlane.Install()
-	assert.Nil(t, err)
+	// err = fastlane.Install()
+	// assert.Nil(t, err)
 
 	// Reset CI flag
-	os.Unsetenv("GITLAB_CI")
+	// os.Unsetenv("GITLAB_CI")
 
 	// Bring gem back to original state
-	if installed == true {
-		err := g.InstallSimple("fastlane")
-		assert.Nil(t, err)
-	}
+	// if installed == true {
+	// err := g.InstallSimple("fastlane")
+	// assert.Nil(t, err)
+	// }
 }
