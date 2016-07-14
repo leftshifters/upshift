69edd622e6af772e7a893c2bf5e0f86091c2b107
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-14 18:55:49 +0530

    Gitlab cli requires terminals to use utf-8 encoding. Hence exporting LANG

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/leftshifters/upshift/tasks"
 	"github.com/leftshifters/upshift/utils"
 )
@@ -8,5 +10,6 @@ import (
 // Main Function
 func main() {
 	utils.SetupFolders()
+	os.Setenv("LANG", "en_US.UTF-8")
 	tasks.Setup()
 }
