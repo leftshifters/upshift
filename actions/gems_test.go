4a4d464f680ab51c8c0f517b437d47f72b58ac27
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-14 19:00:58 +0530

    Only adding tests which can run on CI

--- actions/gems_test.go
+++ actions/gems_test.go
@@ -15,27 +15,15 @@ func Test_Gem_Install(t *testing.T) {
 	// Check if it is installed or not
 	installed, _ := g.version(gem)
 
+	// Change env setting, simulate CI
+	os.Setenv("GITLAB_CI", "true")
+
 	// Uninstall gem first
 	err := g.UninstallSimple(gem)
 	assert.Nil(t, err)
 
 	// Install gem
 	err = g.InstallSimple(gem)
-	assert.Nil(t, err)
-
-	// Uninstall it again
-	err = g.UninstallSimple(gem)
-	assert.Nil(t, err)
-
-	// Change env setting, simulate CI
-	os.Setenv("GITLAB_CI", "true")
-
-	// Install gem in CI
-	err = g.InstallSimple(gem)
-	assert.Nil(t, err)
-
-	// Uninstall gem in CI
-	err = g.UninstallSimple(gem)
 	assert.Nil(t, err)
 
 	// Reset CI flag
