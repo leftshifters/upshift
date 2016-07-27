3efaefcf2ffdc92d6e7a5235a90966ab7d4313e9
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-28 00:53:45 +0530

    Used iso-test-swift for testing pods

--- actions/pod_test.go
+++ actions/pod_test.go
@@ -1,6 +1,8 @@
 package actions
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,11 +11,13 @@ import (
 func Test_Pod_All(t *testing.T) {
 	var p Pod
 
+	os.Chdir(filepath.Join("..", "ios-test-swift"))
+
 	installed := p.IsInstalled()
 	assert.Equal(t, true, installed)
 
 	used := p.AreUsed()
-	assert.Equal(t, false, used)
+	assert.Equal(t, true, used)
 
 	status, err := p.Install()
 	assert.Equal(t, 0, status)
