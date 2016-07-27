c76ca7c447c961d4bbf86691709bcca0df6fb5f5
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-28 01:12:16 +0530

    Removed logs from test.sh

--- test.sh
+++ test.sh
@@ -4,16 +4,11 @@ set -e
 echo "" > coverage.txt
 
 for d in $(find ./* -maxdepth 10 -type d | grep -v actions); do
-    printf "Testing $d\n"
     if ls $d/*.go &> /dev/null; then
-        printf "Checking $d\n"
         go test -v -timeout 99999s -coverprofile=profile.out -covermode=atomic $d
         if [ -f profile.out ]; then
-            printf "$d/profile.out moving to coverage.txt\n"
             cat profile.out >> coverage.txt
             rm profile.out
-        else
-            printf "$d/profile.out not found\n"
         fi
     fi
 done
